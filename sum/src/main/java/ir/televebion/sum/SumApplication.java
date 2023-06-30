7327b3b78383f61b039bb87ec7e850fb9b26b440
ms.rafiee <srafiee@dpco.net>
2023-06-30 22:29:01 +0330

    first init commit projects

---
+++ sum/src/main/java/ir/televebion/sum/SumApplication.java
@@ -0,0 +1,13 @@
+package ir.televebion.sum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumApplication.class, args);
+	}
+
+}
